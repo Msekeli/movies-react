040989edb99099cafb6961e4b70d1c2bdb78361b
Msekeli Mkwibiso <msekeli14@gmail.com>
2023-08-21 15:21:22 +0200

    Top finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+// importing css
+import './App.css';
+import SearchIcon from './search.svg';
+const API_URL = 'http://www.omdbapi.com?apikey=4a04ea76';
 
 const  App = () => {
+    const searchMovie = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data);
+    }
+    useEffect(() =>{
+      searchMovie('Spiderman');
+    },[]);
+
     return (
-        <h1>App</h1>
-    );
+       <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+            <input 
+            placeholder="find movie" 
+            value="Spiderman"
+            onChange={() => {}}
+            />
+            <img src={SearchIcon} alt="Movie image"
+            onClick={() => {}}
+            />
+        </div>
+
+        <div className="container">
+            
+        </div>
+       </div>
+    );  
 }
 
 export default App;
